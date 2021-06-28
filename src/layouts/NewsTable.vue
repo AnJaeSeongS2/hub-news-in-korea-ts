a4f33127fcd2e76913a5131e08204a1ff9fec0ed
AnJaeSeongS2 <jindam91@naver.com>
2021-06-28 23:39:29 +0900

    [NewsTables] rows per page converting.

--- src/layouts/NewsTable.vue
+++ src/layouts/NewsTable.vue
@@ -4,26 +4,50 @@
       title="News(Tistorys) Table"
       :rows="newsList"
       :columns="columns"
-      :class="classObject"
+      :filter="filter"
+      :pagination="myInitialPagination"
       row-key="guid"
-    />
+    >
+      <template v-slot:top-right>
+        <q-input
+          borderless
+          dense
+          debounce="300"
+          v-model="filter"
+          placeholder="Search"
+        >
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+    </q-table>
   </div>
 </template>
 <script lang="ts">
 import Parser = require('rss-parser');
 import moment = require('moment');
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 // no use strict mode.
 // const parser = new Parser({ xml2js: { strict: false } });
 const parser = new Parser();
 const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
 moment.locale('ko-kr');
 
+interface Row {
+  title: string;
+  pubDate: moment.MomentInput;
+}
+
 export default defineComponent({
   name: 'news-table',
   props: {},
   data() {
     return {
+      myInitialPagination: {
+        rowsPerPage: 10,
+      },
+      filter: ref(''),
       newsList: [],
       columns: [
         {
@@ -31,7 +55,7 @@ export default defineComponent({
           required: true,
           label: 'Title',
           align: 'left',
-          field: (row: any) => row.title,
+          field: (row: Row) => row.title,
           format: (val: string) => `${val}`,
           sortable: true,
         },
@@ -40,8 +64,10 @@ export default defineComponent({
           required: true,
           label: 'Published Date',
           align: 'right',
-          field: (row: any) => row.pubDate,
-          format: (val: moment.MomentInput) => `${this.fromNow(val)}`,
+          field: (row: Row) => row.pubDate,
+          format: (val: moment.MomentInput): string => {
+            return this.fromNow(val) as string;
+          },
           sortable: true,
         },
       ],
@@ -58,7 +84,7 @@ export default defineComponent({
     this.newsList = response.items as never;
   },
   methods: {
-    fromNow(timestamp: moment.MomentInput) {
+    fromNow(timestamp: moment.MomentInput): string {
       return moment(timestamp).fromNow();
     },
   },
